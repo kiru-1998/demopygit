ec48b6df2d9d7ace9bec4dfbe49cae896f50e18c
Kiru-1998 <kiran146172@gmail.com>
2024-11-06 11:01:52 +0530

    special case added

---
+++ calc.py
@@ -0,0 +1,15 @@
+
+def add():
+    print('result1 is'+ __name__)
+def sub():
+    print('result2 is ')
+
+def main():
+    print('i need calc')
+    add()
+    sub()
+
+if __name__ == '__main__':
+    main()
+
+
